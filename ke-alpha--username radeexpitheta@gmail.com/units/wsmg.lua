fb0744fd125e31aea70154f7e24f9aacaa11bbd8
radeexpitheta@gmail.com <radeexpitheta@gmail.com@b1b02279-7c3a-6898-24be-2e39748f2050>
2011-12-26 07:17:41 +0000

    a little closer, but some strange bugs still around in soldier.lua

---
+++ ke-alpha--username radeexpitheta@gmail.com/units/wsmg.lua
@@ -0,0 +1,59 @@
+-- UNITDEF -- wsmg --
+--------------------------------------------------------------------------------
+
+local unitName = "wsmg"
+
+--------------------------------------------------------------------------------
+
+local unitDef = {
+	unitname           = [[wsmg]],
+	name               = [[Sub Machinegun]],
+	description        = [[Weapon]],	
+	--iconType		    = "weapon",
+   -- acceleration        = 0,
+  --bmcode              = [[1]],
+  --brakeRate           = 0,
+  buildCostMetal     = 0,
+  canMove 			 = true,
+  canstop            = true,
+  category           = [[NOTARGET]],
+  capturable = false,	
+  customParams = {
+		isweapon = 1,
+		},	
+  defaultmissiontype    = [[Standby]],
+  explodeAs          = [[NullDeath]],
+  footprintX         = 1,
+  footprintZ         = 1,
+  hidedamage		 = 1,
+  maneuverleashlength = [[1]],
+  mass				 = 5,
+  maxDamage          = 8000,
+  maxSlope           = 255,
+  maxVelocity         = 0.0000001,
+  maxWaterDepth       = 10,
+  movementClass       = [[Foot]],
+  moveState           = 0,
+  noAutoFire          = false,
+  noChaseCategory     = [[NOTARGET]],
+  objectName         = [[wsmg.s3o]],
+  pushResistant      = true,
+  radarDistance      = 0,
+  script             = [[weapon.lua]],
+  selfDestructAs     = [[NullDeath]],
+  selfDestructCountdown = 0,
+  showNanoFrame      = false,
+  sightDistance      = 0,
+  smoothAnim          = true,
+  steeringmode        = [[1]],
+  TEDClass            = [[TANK]],
+  TurnRate 			 = 1,
+
+}
+
+
+--------------------------------------------------------------------------------
+
+return lowerkeys({ [unitName] = unitDef })
+
+--------------------------------------------------------------------------------
